41085b5c9f6074e488b9f85fa35c73dbbf70d685
Chaosg5 <erik.bunnstad@evry.com>
2019-10-21 08:51:31 +0200

    Added a Wedding project

---
+++ Wedding/Chaos.Wedding/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chaos.Wedding.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inbjudan()
+        {
+            ViewBag.Message = "Inbjudan";
+
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+            ViewBag.Message = "Information";
+
+            return View();
+        }
+
+        public ActionResult Presenter()
+        {
+            ViewBag.Message = "Information";
+
+            return View();
+        }
+    }
+}
---
+++ Wedding/Chaos.Wedding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chaos.Wedding.MvcApplication" Language="C#" %>
